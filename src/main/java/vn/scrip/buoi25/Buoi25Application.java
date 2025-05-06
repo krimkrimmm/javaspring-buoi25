55c9632a60251e8fae3b0ed1279d07aacf5df03b
krimkrimmm <147626046+krimkrimmm@users.noreply.github.com>
2025-05-06 17:33:49 +0700

    javaspring-buoi25

---
+++ src/main/java/vn/scrip/buoi25/Buoi25Application.java
@@ -0,0 +1,19 @@
+//Yêu cầu
+//Tạo giao diện trang đăng nhập
+//API URL: /dang-nhap
+//Xử lý phần đăng nhập trên giao diện. Gọi đến API /api/auth/login như đã hướng dẫn trên lớp
+
+package vn.scrip.buoi25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class Buoi25Application
+{
+	public static void main(String[] args) {
+		SpringApplication.run(Buoi25Application.class, args);
+	}
+}
+
+
+
